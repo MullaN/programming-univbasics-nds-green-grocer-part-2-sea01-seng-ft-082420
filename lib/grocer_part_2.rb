43c11f2f2a830e0bd6e6f7a2fea717dedd8b4330
Andrew Mullan <admullan@gmail.com>
2020-08-18 06:26:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,28 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart = consolidate_cart(cart)
+  binding.pry
+  coupons.each do |coupon_item|
+    cart_num = cart.find_index {|cart_item| cart_item[:name] == coupon_item[:name]}
+    if cart_num && (cart[cart_num][:count] >= coupon_item[:num])
+      cart << {item: "#{cart[cart_num][:name]} W/ COUPON", price: coupon_item[:cost] / coupon_item[:num], clearance: cart[cart_num][:clearance], count: (cart[cart_num][:count] / coupon_item[:num]).to_i}
+      cart[cart_num][:count] = cart[cart_num][:count] % coupon_item[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart.map { |cart_item| cart_item[:price] *= 0.8 if cart_item[:clearance] == true}
+  cart
 end
 
 def checkout(cart, coupons)
