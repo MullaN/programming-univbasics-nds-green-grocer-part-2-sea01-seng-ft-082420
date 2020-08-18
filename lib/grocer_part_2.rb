ee47575d21568c3030f1929941d93fdf64e81773
Andrew Mullan <admullan@gmail.com>
2020-08-18 06:56:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -9,6 +9,11 @@ def apply_coupons(cart, coupons)
     cart_num = cart.find_index {|cart_item| cart_item[:name] == coupon_item[:name]}
     if cart_num && cart[cart_num][:count] >= coupon_item[:num]
       cart << {item: "#{cart[cart_num][:item]} W/COUPON", price: coupon_item[:cost] / coupon_item[:num], clearance: cart[cart_num][:clearance], count: (cart[cart_num][:count] / coupon_item[:num]).to_i * coupon_item[:num]}
+      if cart[cart_num][:count] % coupon_item[:num] == 0
+        cart.delete_at(cart_num)
+      else
+        cart[cart_num][:count] %= coupon_item[:num]
+      end
     end
   end
   cart
