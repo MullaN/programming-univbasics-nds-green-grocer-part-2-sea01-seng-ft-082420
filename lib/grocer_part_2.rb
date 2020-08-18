647e9e13407621c8b51e833fbc471da7736c59d7
Andrew Mullan <admullan@gmail.com>
2020-08-18 07:18:06 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -19,7 +19,7 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  cart.map { |cart_item| cart_item[:price] *= 0.8 if cart_item[:clearance] == true}
+  cart.map { |cart_item| (cart_item[:price] *= 0.8).to_d if cart_item[:clearance] == true}
   cart
 end
 
